df455270cebfa56cbc16c0d4e8dea19ec711f914
bertman12 <eddydaniel94@gmail.com>
2024-01-06 08:01:29 -0800

    fixed bugs; need to account for illegal characters in names and duplicate names

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -17,9 +17,9 @@ try {
     fs.writeFileSync("src/war3map.d.ts", result);
 
     const wtsFileContents = fs.readFileSync(wtsFile, "utf8");
-    const alreadyCreated = fs.pathExistsSync("src/parser-test");
+    const folderExists = fs.pathExistsSync("src/parser-test");
 
-    if (alreadyCreated) {
+    if (folderExists) {
         generateEnumsFromWTS(wtsFileContents);
     } else {
         fs.mkdirSync("src/parser-test");
@@ -30,26 +30,25 @@ try {
     logger.error(`There was an error generating the definition file for '${luaFile}'`);
 }
 
-type WTS_ObjectTypes = "Units" | "Items" | "Abilities" | "Buffs" | "Upgrades";
-type WTS_LineMetaType = "Name" | "Tip" | "Ubertip";
-
-enum LineWordIndexMap {
-    StartIdentifier,
-    ObjectType,
-    FourCC,
-    ObjectName,
-    StringTypeShort, //tip, ubertip, name
-    StringType, //Name, Tooltip, Tooltip Extended
-}
-
 function generateEnumsFromWTS(fileContents: string) {
     let newFileContents = "";
+    type WTS_ObjectTypes = "Units" | "Items" | "Abilities" | "Buffs" | "Upgrades";
+    type WTS_LineMetaType = "Name" | "Tip" | "Ubertip";
 
+    enum LineWordIndexMap {
+        StartIdentifier,
+        ObjectType,
+        FourCC,
+        ObjectName,
+        StringTypeShort, //tip, ubertip, name
+        StringType, //Name, Tooltip, Tooltip Extended
+    }
     //For fucking around
     const charReplacementMap = new Map<string, string>([
         [" ", "-space-"],
         ["\n", "-new-line-"],
         ["\t", "-tab-"],
+        ["-", "_"],
     ]);
 
     //To be concatenated together for the new file contents for generated enums
@@ -96,11 +95,23 @@ function generateEnumsFromWTS(fileContents: string) {
      */
 
     // ["//", "Units:", "I000", "(Object Name)", "Name", "(Name)"]
-    let currentLine: string[] = [];
+    let currentLineWords: string[] = [];
     let lastReadWord = "";
     // let isReadingMultiWordObjectName = false;
     let concatenatingWordsInProgress = false;
     let firstCompositeWordAdded = false;
+
+    /**
+     * What I think its doing
+     * 0 - lastReadWord = //
+     * 1 - lastReadWord = Units:
+     * 2 - lastReadWord = h000
+       3 -  * lastReadWord = (My
+       3 -  * lastReadWord = Object
+       3 -  * lastReadWord = Name)
+    *  4 - Name
+    *  5 - (Name)
+     */
 
     //This does preserve the line breaks and the spaces
     for (let x = 0; x < fileContents.length; x++) {
@@ -109,25 +120,39 @@ function generateEnumsFromWTS(fileContents: string) {
 
         //At this point, we might have all the relevant data to create the new enum member, now we simple check our array of words for the line
         if (char === "\n") {
-            newFileContents += "\n//";
-            currentLine.forEach((word) => {
-                newFileContents += word;
-            });
+            // if (currentLine.length > 1) {
+            //     newFileContents += `\n//`;
+            //     currentLine.forEach((word, index) => {
+            //         newFileContents += `(index: ${index}) ${word} `;
+            //     });
+            // }
 
-            if (currentLine.includes("Name")) {
-                newFileContents += "\n//current line includes name";
+            if (currentLineWords.includes("Name") && currentLineWords[3]) {
+                currentLineWords[3] = currentLineWords[3]?.replace("(", "");
+                currentLineWords[3] = currentLineWords[3]?.replace(")", "");
+                currentLineWords[3] = currentLineWords[3]?.replace(",", "");
 
-                const newLine = `\n${currentLine[3]} = FourCC(${currentLine[2]}),`;
-                objectTypesEnumStrings.set(currentLine[1] as WTS_ObjectTypes, newLine);
-               
+                const pattern = new RegExp("[a-z]");
+                for (let x = 0; x < currentLineWords[3].length; x++) {
+                    const c = currentLineWords[3][x];
+
+                    if (!pattern.test(c)) {
+                        //Replace the illegal character
+                        currentLineWords[3].replace(currentLineWords[3][x], "_");
+                    }
+                }
+
+                const newLine = `\n${currentLineWords[3]} = FourCC("${currentLineWords[2]}"),`;
+
+                const objectType = objectTypeIdentifier.get(currentLineWords[1]);
+                const currentValue = objectTypesEnumStrings.get(objectType as WTS_ObjectTypes);
+
+                objectTypesEnumStrings.set(objectType as WTS_ObjectTypes, currentValue + newLine);
             }
 
-                currentLine = [];
+            //Current line is reset once we reach new line symbol
+            currentLineWords = [];
             lastReadWord = "";
-
-            // if (currentLine.length > 0) {
-            //     newFileContents += `\n//Current line length: ${currentLine.length}`;
-            // }
         }
 
         //means we have reached the end of the current word. here is where we check the word to orient ourself in the file contents
@@ -138,41 +163,62 @@ function generateEnumsFromWTS(fileContents: string) {
             //detect if we need to concatenate words
             if (shouldConcatWords(lastReadWord)) {
                 concatenatingWordsInProgress = true;
-                newFileContents += "\n//We are concatenating words now";
-            } else if (!concatenatingWordsInProgress) {
-                currentLine.push(lastReadWord);
             }
 
+            //We have already started making a composite word which means the last item in the array is our composite word
+            if (concatenatingWordsInProgress && firstCompositeWordAdded) {
+                //Add last read word to it
+                currentLineWords[currentLineWords.length - 1] += lastReadWord;
+            }
+
+            //If we are adding word but haven't started yet, then push the first word into the array
             if (concatenatingWordsInProgress && !firstCompositeWordAdded) {
                 //This is the first word in the sequence, therefore push to the array
-                currentLine.push(lastReadWord);
+                currentLineWords.push(lastReadWord);
                 firstCompositeWordAdded = true;
             }
 
-            //We have already begun our composite word sequence
-            if (concatenatingWordsInProgress && firstCompositeWordAdded) {
-                //Add last read word to it
-                currentLine[currentLine.length - 1] += lastReadWord;
+            /**
+             * on our next pass, if we have just completed a composite word, then we can start pushing new items in the array as normal
+             */
+            if (!concatenatingWordsInProgress) {
+                currentLineWords.push(lastReadWord);
             }
 
             //Check if we still need to concatenate words, we are done if its an end parentheses and theres a space after it.
-            if (lastReadWord.includes(")") && nextChar === " ") {
+            //Example word is Name) therefore we should not push this word into the array it should just be added the last item in the array to complete
+            //the composite word
+            if (lastReadWord.includes(")") || lastReadWord.includes("),")) {
                 concatenatingWordsInProgress = false;
+                firstCompositeWordAdded = false;
             }
 
             //reset word after we have made use of it
             lastReadWord = "";
-        } else {
+        } else if (char !== "\n") {
             //Building the next word
             lastReadWord += char;
         }
     }
 
+    /**
+     * What I think its doing
+     * 0 - lastReadWord = //
+     * 1 - lastReadWord = Units:
+     * 2 - lastReadWord = h000
+       3 -  * lastReadWord = (My
+       3 -  * lastReadWord = Object
+       3 -  * lastReadWord = Name)
+    *  4 - Name
+    *  5 - (Name)
+     */
     //Now we create our enums with the string data we parsed
     for (const [key, value] of objectTypesEnumStrings) {
         const newEnumName = objectTypeEnumNames.get(key);
 
-        newFileContents += `\nexport enum ${newEnumName}{\n${value}\n}`;
+        if (key && value) {
+            newFileContents += `\nexport enum ${newEnumName}{\n${value}\n}`;
+        }
     }
 
     fs.writeFileSync("src/parser-test/GeneratedEnums.ts", newFileContents);
--- src/parser-test/GeneratedEnums.ts
+++ src/parser-test/GeneratedEnums.ts
@@ -1,556 +1,115 @@
 
-//﻿STRING
-//
-//
-Magic
-//
-//
-//
-STRING
-//
-//
-//
-//
-//
-STRING
-//
-//
-//
-//
-//
-STRING
-//
-//
-//
-//
-//
-STRING
-//
-//
-Magic
-//
-//
-//
-STRING
-//
-//
-Player
-//
-//
-//
-STRING
-//
-//
-Force
-//
-//
-//
-STRING
-//We are concatenating words now
-//
-//Units:H000(Blood(BloodElfSurvivor),Name
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//We are concatenating words now
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//We are concatenating words now
-//
-//
-//
-//
-//
+//(index: 0) // (index: 1) Units: (index: 2) H000 (index: 3) (BloodElfSurvivor), (index: 4) Name 
+//(index: 0) Blood (index: 1) Elf 
+//(index: 0) // (index: 1) Items: (index: 2) I000 (index: 3) (KineticBlade), (index: 4) Name 
+//(index: 0) // (index: 1) Items: (index: 2) I000 (index: 3) (KineticBlade), (index: 4) Ubertip (index: 5) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Ubertip (index: 1) (Tooltip- 
+//(index: 0) Mana (index: 1) Core 
+//(index: 0) // (index: 1) Units: (index: 2) u000 (index: 3) (DepletedManaCore), (index: 4) Name 
+//(index: 0) Depleted (index: 1) Mana 
+//(index: 0) // (index: 1) Units: (index: 2) n001 (index: 3) (DefunctTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h001 (index: 3) (Wall), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h003 (index: 3) (ImprovedWall1), (index: 4) Name 
+//(index: 0) Improved (index: 1) Wall 
+//(index: 0) // (index: 1) Units: (index: 2) h002 (index: 3) (ManaWall1), (index: 4) Name 
+//(index: 0) Mana (index: 1) Wall 
+//(index: 0) // (index: 1) Units: (index: 2) h004 (index: 3) (TowerBase), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h006 (index: 3) (ManaTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h007 (index: 3) (CripplingTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h008 (index: 3) (SummoningTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h009 (index: 3) (LightningTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h00A (index: 3) (ArmorPiercingTower), (index: 4) Name 
+//(index: 0) Armor (index: 1) Piercing 
+//(index: 0) // (index: 1) Units: (index: 2) h00B (index: 3) (MultishotTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h00C (index: 3) (FreezingTower), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h001 (index: 3) (Wall), (index: 4) Tip (index: 5) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) Tip (index: 1) (Tooltip- 
+//(index: 0) // (index: 1) Units: (index: 2) h00D (index: 3) (FlameWall), (index: 4) Tip (index: 5) (Tooltip- 
+//(index: 0) // (index: 1) Units: (index: 2) h002 (index: 3) (ManaWall1), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h00A (index: 3) (ArmorPiercingTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h003 (index: 3) (ImprovedWall1), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h00C (index: 3) (FreezingTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h006 (index: 3) (ManaTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h007 (index: 3) (CripplingTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h008 (index: 3) (SummoningTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h009 (index: 3) (LightningTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h00B (index: 3) (MultishotTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h005 (index: 3) (GuardTower), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) h00D (index: 3) (FlameWall), (index: 4) Hotkey 
+//(index: 0) // (index: 1) Units: (index: 2) n002 (index: 3) (Villager), (index: 4) Tip (index: 5) (Tooltip- 
+//(index: 0) Ubertip (index: 1) (Tooltip- 
+//(index: 0) EditorSuffix (index: 1) (Name-Editor 
+//(index: 0) // (index: 1) Items: (index: 2) I001 (index: 3) (PersonalManaShield-I), (index: 4) Name 
+//(index: 0) Personal (index: 1) Mana (index: 2) Shield (index: 3) - 
+//(index: 0) // (index: 1) Items: (index: 2) I002 (index: 3) (PersonalFountain-I), (index: 4) Name 
+//(index: 0) Personal (index: 1) Fountain (index: 2) - 
+//(index: 0) // (index: 1) Abilities: (index: 2) A000 (index: 3) (Multishot), (index: 4) Name 
+//(index: 0) // (index: 1) Abilities: (index: 2) A000 (index: 3) (Multishot), (index: 4) EditorSuffix (index: 5) (Editor 
+//(index: 0) Multishot (index: 1) Item (index: 2) - 
+//(index: 0) // (index: 1) Items: (index: 2) I004 (index: 3) (FlameShield-I), (index: 4) Name 
+//(index: 0) Flame (index: 1) Shield (index: 2) - 
+//(index: 0) // (index: 1) Items: (index: 2) I005 (index: 3) (FlameRune), (index: 4) Name 
+//(index: 0) // (index: 1) Abilities: (index: 2) A001 (index: 3) (FlameRune), (index: 4) Name 
+//(index: 0) // (index: 1) Abilities: (index: 2) A001 (index: 3) (FlameRune), (index: 4) EditorSuffix (index: 5) (Editor 
+//(index: 0) // (index: 1) Abilities: (index: 2) A002 (index: 3) (Teleport), (index: 4) Tip (index: 5) (Tooltip- 
+//(index: 0) Ubertip (index: 1) (Tooltip-Normal- 
+//(index: 0) Hotkey (index: 1) (Hotkey- 
+//(index: 0) // (index: 1) Upgrades: (index: 2) R001 (index: 3) (Chaos), (index: 4) Name 
+//(index: 0) // (index: 1) Upgrades: (index: 2) R002 (index: 3) (LongRifles), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h00F (index: 3) (ItemShop), (index: 4) Name 
+//(index: 0) // (index: 1) Upgrades: (index: 2) R003 (index: 3) (WellSpring), (index: 4) Name 
+//(index: 0) Improved (index: 1) Mana 
+//(index: 0) // (index: 1) Upgrades: (index: 2) R004 (index: 3) (Chaos), (index: 4) Name 
+//(index: 0) Advanced (index: 1) Item 
+//(index: 0) // (index: 1) Upgrades: (index: 2) R005 (index: 3) (Chaos), (index: 4) Name 
+//(index: 0) // (index: 1) Units: (index: 2) h00G (index: 3) (testingparser), (index: 4) Name 
 export enum WTS_Units{
 
+BloodElfSurvivor = FourCC("H000"),
+DepletedManaCore = FourCC("u000"),
+DefunctTower = FourCC("n001"),
+Wall = FourCC("h001"),
+ImprovedWall1 = FourCC("h003"),
+ManaWall1 = FourCC("h002"),
+TowerBase = FourCC("h004"),
+ManaTower = FourCC("h006"),
+CripplingTower = FourCC("h007"),
+SummoningTower = FourCC("h008"),
+LightningTower = FourCC("h009"),
+ArmorPiercingTower = FourCC("h00A"),
+MultishotTower = FourCC("h00B"),
+FreezingTower = FourCC("h00C"),
+ItemShop = FourCC("h00F"),
+testingparser = FourCC("h00G"),
 }
 export enum WTS_Abilities{
 
+Multishot = FourCC("A000"),
+FlameRune = FourCC("A001"),
 }
 export enum WTS_Items{
 
-}
-export enum WTS_Buffs{
-
+KineticBlade = FourCC("I000"),
+PersonalManaShield-I = FourCC("I001"),
+PersonalFountain-I = FourCC("I002"),
+FlameShield-I = FourCC("I004"),
+FlameRune = FourCC("I005"),
 }
 export enum WTS_Upgrades{
 
+Chaos = FourCC("R001"),
+LongRifles = FourCC("R002"),
+WellSpring = FourCC("R003"),
+Chaos = FourCC("R004"),
+Chaos = FourCC("R005"),
 }
